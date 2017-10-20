3e70bdf20e37059a849e9f43690aa65ce0acf78d
Ory Medina <ory@wizeservices.com>
2017-10-20 01:59:55 -0500

    Add post method and send message method

--- src/controllers/facebook.js
+++ src/controllers/facebook.js
@@ -18,8 +18,76 @@ const getWebhook = (req, res) => {
       res.sendStatus(403);
     }
   }
-}
+};
+
+const postWebhook = (req, res) => {
+  const data = req.body;
+
+  // Make sure it's coming from a page
+  if (data.object === 'page') {
+    data.entry.forEach(entry => {
+      const pageId = entry.id;
+      const timeOfEvend = entry.time;
+
+      // Now each of the messaging events
+      entry.messaging.forEach(event => {
+        if(event.message) {
+          receivedMessage(event);
+        } else {
+          console.log('Received an unknown event', event);
+        }
+      });
+    });
+  }
+
+  // Then we lie and tell facebook everything went fine and dandy
+  res.sendStatus(200);
+};
+
+const receivedMessage = (event) => {
+  const senderId = event.sender.id;
+  const recipientId = event.recipient.id;
+  const messageId = event.message.id;
+  const text = event.message.text;
+
+  // Do something magical
+
+  sendTextMessage(senderId, text);
+};
+
+const sendTextMessage = (senderId, text) => {
+  const message = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text
+    }
+  };
+
+  callSendAPI(message);
+};
+
+const callSendAPI = (message) => {
+  requestPromise({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: process.env.PAGE_ACCESS_TOKEN
+    },
+    method: 'POST',
+    json: message
+  }).then(response => {
+    if(response.statusCode === 200) {
+      const {recipientId, messageId} = response
+
+      console.log(`Message: ${messageId} sent to ${recipientId}`);
+    }
+  }).catch(error => {
+    console.error('Unable to send message');
+    console.error(error);
+  });
+};
 
 module.exports = {
   getWebhook
-}
+};
